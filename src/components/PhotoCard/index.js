55c15278a04a1c2db43a994aecea3723704791bf
SirNahe <47546713+SirNahe@users.noreply.github.com>
2020-02-21 19:00:00 -0400

    Fix likes

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -2,17 +2,14 @@ import React, { useEffect, useRef, useState } from 'react'
 import { Article, ImgWrapper, Img } from './styles'
 import { FavButton } from '../FavButton'
 import { ToggleLikeMutation } from '../../container/ToggleLikeMutation'
-import { useLocalStorage } from '../../hooks/useLocalStorage'
 import { useNearScreen } from '../../hooks/useNearScreen'
 import { Link } from '@reach/router'
 
 const DEFAULT_IMAGE =
   'https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png'
 
-export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
+export const PhotoCard = ({ id, liked, likes = 0, src = DEFAULT_IMAGE }) => {
   const [show, ref] = useNearScreen()
-  const photoKey = `like-${id}`
-  const [liked, setLiked] = useLocalStorage(photoKey, false)
 
   return (
     <Article ref={ref}>
@@ -26,8 +23,7 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
           <ToggleLikeMutation>
             {toggleLike => {
               const handleFavClick = () => {
-                !liked && toggleLike({ variables: { input: { id } } })
-                setLiked(!liked)
+                toggleLike({ variables: { input: { id } } })
               }
 
               return (
